f9435628279c93fca6fcbb63b9afb7846a0de71a
Chipster <go@w.ay>
2023-12-20 11:02:10 -0600

    update

--- usr-local-cast-sbin/flash_next.sh
+++ usr-local-cast-sbin/flash_next.sh
@@ -6,13 +6,9 @@
 # PE1MSZ, PE1PLM, W0CHP
 #
 
-# let's only stop what's neccessary...
-sudo systemctl stop cron.service  > /dev/null 2>&1
-sudo systemctl stop pistar-watchdog.timer > /dev/null 2>&1
-sudo systemctl stop pistar-watchdog.service > /dev/null 2>&1
-sudo systemctl stop mmdvmhost.timer > /dev/null 2>&1
-sudo systemctl stop mmdvmhost.service > /dev/null 2>&1
-sudo systemctl stop castserial.service > /dev/null 2>&1
+sudo wpsd-services fullstop
+
+sleep 5
 
 # firmware received in zip-format, unzip and continue
 DIR="/opt/cast/usr-local-cast-www/cast-firmware/fw/nextion"
@@ -41,12 +37,6 @@ do
     sleep 2 
     sudo /usr/local/cast/bin/cast-reset
     sleep 2 
-
-    sudo systemctl start castserial.service > /dev/null 2>&1
-    sudo systemctl start mmdvmhost.service > /dev/null 2>&1
-    sudo systemctl start mmdvmhost.timer > /dev/null 2>&1
-    sudo systemctl start pistar-watchdog.service > /dev/null 2>&1
-    sudo systemctl start pistar-watchdog.timer > /dev/null 2>&1
     sudo systemctl start cron.service > /dev/null 2>&1
 done
 
