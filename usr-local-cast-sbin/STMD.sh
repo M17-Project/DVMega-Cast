c20b00814322ee7f722a09d87333a46c83c05bd4
Chipster <go@w.ay>
2023-12-22 07:24:58 -0600

    tes

--- usr-local-cast-sbin/STMD.sh
+++ usr-local-cast-sbin/STMD.sh
@@ -3,7 +3,7 @@
 #
 # Sets All modes to what boolean is passed to the argument:
 # 
-# STMD.sh DMR(0,1) D-Star(0,1) YSF(0,1)
+# STMD.sh D-Star(0,1) DMR (0|1) YSF(0,1)
 # 
 #   This script is used by the Cast display udp service when
 #   a user enables/disabled their delected modes.
@@ -11,10 +11,8 @@
 # PE1MSZ, PE1PLM, W0CHP
 #
 
-if [ "$#" -ne 3 ]; then
-    echo "Bad syntax"
-    exit 1
-fi
+# log invocations:
+echo "Command: $0 $@" > /tmp/stmd.log
 
 MMDVMCONFIG="/etc/mmdvmhost"
 
@@ -24,8 +22,8 @@ DStar_e=$(awk -F'=' '/\[D-Star\]/{flag=1; next} flag && /Enable/{gsub(/[[:space:
 YSF_e=$(awk -F'=' '/\[System Fusion\]/{flag=1; next} flag && /Enable/{gsub(/[[:space:]]/, "", $2); print $2; flag=0}' ${MMDVMCONFIG})
 
 # map STMD.sh's 3 args
-arg1=$1  # DMR
-arg2=$2  # D-Star
+arg1=$1  # D-Star
+arg2=$2  # DMR
 arg3=$3  # YSF
 
 # Convert STMD's mode status boolean args to wpsd-mode-manager's mode status string args
@@ -36,20 +34,23 @@ convert_to_status() {
     esac
 }
 # now map the mode status booleans to strings for wpsd-mode-manager using convert_to_status()
-DMR_s=$(convert_to_status "$arg1")
-DStar_s=$(convert_to_status "$arg2")
+DStar_s=$(convert_to_status "$arg1")
+DMR_s=$(convert_to_status "$arg2")
 YSF_s=$(convert_to_status "$arg3")
 
 # check if the desired status is different from the current status before calling wpsd-mode-manager
-if [ "$DMR_s" != "$DMR_e" ]; then
-    sudo /usr/local/sbin/wpsd-mode-manager "DMR" "$DMR_s"
-fi
-
-if [ "$DStar_s" != "$DStar_e" ]; then
+if [ "$arg1" != "$DStar_e" ]; then
+    sudo /usr/local/cast/bin/cast-reset
     sudo /usr/local/sbin/wpsd-mode-manager "D-Star" "$DStar_s"
 fi
 
-if [ "$YSF_s" != "$YSF_e" ]; then
+if [ "$arg2" != "$DMR_e" ]; then
+    sudo /usr/local/cast/bin/cast-reset
+    sudo /usr/local/sbin/wpsd-mode-manager "DMR" "$DMR_s"
+fi
+
+if [ "$arg3" != "$YSF_e" ]; then
+    sudo /usr/local/cast/bin/cast-reset
     sudo /usr/local/sbin/wpsd-mode-manager "YSF" "$YSF_s"
 fi
 
