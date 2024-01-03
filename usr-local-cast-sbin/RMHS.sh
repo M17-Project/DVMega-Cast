3da39f3e36e5c73f3eaa444218b865e37b97a4b9
Chipster <go@w.ay>
2024-01-03 06:33:53 -0600

    fix hotspot UARTspeed entry

--- usr-local-cast-sbin/RMHS.sh
+++ usr-local-cast-sbin/RMHS.sh
@@ -8,6 +8,7 @@ sudo systemctl stop mmdvmhost.service > /dev/null 2>&1
 
 sudo sed -i "/\[Modem\]/,/\[/ s/Hardware=.*$/Hardware=dvmpicasths/1" /etc/dstar-radio.mmdvmhost
 sudo sed -i "/\[Modem\]/,/\[/ s/Port=.*$/Port=\/dev\/ttyS2/1" /etc/mmdvmhost
+sudo sed -i "/\[Modem\]/,/\[/ s/UARTSpeed=.*$/UARTSpeed=115200/1" /etc/mmdvmhost
 # sudo sed -i "s%.*Hardware=dvmpicast.*%Hardware=dvmpicasthd%" /etc/dstar-radio.mmdvmhost
 # sudo sed -i "s%Port=/dev/ttyAMA0%Port=/dev/ttyS2%1" /etc/mmdvmhost
 sudo sed -i "/repeaterType1=/c\\repeaterType1=0" /etc/ircddbgateway
