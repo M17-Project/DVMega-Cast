6d310718d8f5a03ffde9a0de923310d424231313
Chipster <go@w.ay>
2024-01-03 14:26:49 -0600

    Enhance Base/Hotspot mode script logic

--- usr-local-cast-sbin/RMHS.sh
+++ usr-local-cast-sbin/RMHS.sh
@@ -11,12 +11,14 @@ if [ "$1" != "conf_page" ]; then
     sudo systemctl stop mmdvmhost.service > /dev/null 2>&1
 fi
 
-sudo sed -i "/\[Modem\]/,/\[/ s/Hardware=.*$/Hardware=dvmpicasths/1" /etc/dstar-radio.mmdvmhost
+if [ "$2" == "dual" ]; then # this is also called from the config page...
+    sudo sed -i "s%.*Hardware=dvmpicast.*%Hardware=dvmpicasthd%" /etc/dstar-radio.mmdvmhost
+else
+    sudo sed -i "/\[Modem\]/,/\[/ s/Hardware=.*$/Hardware=dvmpicasths/1" /etc/dstar-radio.mmdvmhost
+fi
+
 sudo sed -i "/\[Modem\]/,/\[/ s/Port=.*$/Port=\/dev\/ttyS2/1" /etc/mmdvmhost
 sudo sed -i "/\[Modem\]/,/\[/ s/UARTSpeed=.*$/UARTSpeed=115200/1" /etc/mmdvmhost
-# sudo sed -i "s%.*Hardware=dvmpicast.*%Hardware=dvmpicasthd%" /etc/dstar-radio.mmdvmhost
-# sudo sed -i "s%Port=/dev/ttyAMA0%Port=/dev/ttyS2%1" /etc/mmdvmhost
-sudo sed -i "/repeaterType1=/c\\repeaterType1=0" /etc/ircddbgateway
 
 # reset the bad boy
 # Don't run this if called from config page:
@@ -34,3 +36,4 @@ fi
 sudo systemctl start castserial.service > /dev/null 2>&1 &
 
 exit 0
+
