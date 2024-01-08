a0374699ccb27252e1bb5d602cb899e4dacdc96c
Chipster <go@w.ay>
2024-01-08 03:24:44 -0600

    more cleanups

--- usr-local-cast-sbin/RLST.sh
+++ usr-local-cast-sbin/RLST.sh
@@ -1,24 +1,17 @@
 #!/bin/bash
 
 #
-# Cast Memory list managment
+# Cast Memory list managment; pushes list into cast UDPserver
 #
 # PE1MSZ, PE1PLM, W0CHP
 #
 
-## Not needed since file will always be in /usr/local/cast/etc/castmemlist.txt...
-#FILE=/var/www/dashboard/castmemlist.txt
-#if [ -f "$FILE" ]; then
-    #sudo cp /var/www/dashboard/castmemlist.txt /home/pi-star/castmemlist.txt
-#else
-    #sudo cp /home/pi-star/castmemlist.txt /var/www/dashboard/castmemlist.txt 
-#fi
-
 input="/usr/local/cast/etc/castmemlist.txt"
-((i = 0))
-while IFS= read -r line
-do
-((i=i+1))
-  sudo echo -n  "0,LS$i,$line" >/dev/udp/127.0.0.1/40095
-done < "$input"
 
+((i = 0))
+mapfile -t lines < "$input"
+for line in "${lines[@]}"
+do
+  ((i++))
+  printf "%s" "$line" > /dev/udp/127.0.0.1/40095
+done
