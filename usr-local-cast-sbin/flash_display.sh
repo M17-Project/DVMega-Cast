39e1846ef670bdbf006d2e8b2eff8bb1eeed35c7
Chipster <go@w.ay>
2024-03-21 11:03:18 -0500

    Update nextion flash routines

--- usr-local-cast-sbin/flash_display.sh
+++ usr-local-cast-sbin/flash_display.sh
@@ -14,6 +14,8 @@ sudo systemctl stop mmdvmhost.timer > /dev/null 2>&1
 sudo systemctl stop mmdvmhost.service > /dev/null 2>&1
 sudo systemctl stop castserial.service > /dev/null 2>&1
 
+sudo gpio mode 10 out
+
 # firmware received in zip-format, unzip and continue
 DIR="/opt/cast/usr-local-cast-www/cast-firmware/fw/cast_display"
 UPLOADED="$DIR/*.zip"
@@ -26,10 +28,10 @@ FIRMWARE="$DIR/*.tft"
 for found in $FIRMWARE
 do
     echo "Found $found firmware..."
-
     case ${found} in
-	(*F024*)  python3 /usr/local/cast/sbin/nextionupload.py /dev/ttyAMA0 NX3224F024 ${found};;
-	(*T024*)  python3 /usr/local/cast/sbin/nextionupload.py /dev/ttyAMA0 NX3224T024 ${found};;
+	(*F024*)  python3 python3 /usr/local/cast/sbin/nextionupload.py -p /dev/ttyAMA0 -c 115200 -u 115200 -i ${found};;
+	(*J024*)  python3 python3 /usr/local/cast/sbin/nextionupload.py -p /dev/ttyAMA0 -c 115200 -u 115200 -i ${found};;
+	(*T024*)  python3 python3 /usr/local/cast/sbin/nextionupload.py -p /dev/ttyAMA0 -c 115200 -u 115200 -i ${found};;
     esac
 
     # Make a backup of the uploaded FW to backup-folder
